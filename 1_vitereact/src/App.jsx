76fa63b55d9f48c265203c32ee40cd146fcda089
IsAGi-auRa <itsaman24916@gmail.com>
2026-09-14 15:17:35 +0530

    Learning starting of a Project

---
+++ 1_vitereact/src/App.jsx
@@ -0,0 +1,12 @@
+import Test from './Test.jsx'
+
+function App() {
+  return (
+    <>
+      <h1>Hello Egoist !</h1>
+      <Test />
+    </>
+  )
+}
+
+export default App
---
+++ 1_vitereact/src/Test.jsx
@@ -0,0 +1,7 @@
+function Test(){
+    return (
+        <h3>test is working</h3>
+    )
+}
+
+export default Test
