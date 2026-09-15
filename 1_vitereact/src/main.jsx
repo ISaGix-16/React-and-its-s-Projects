042a0284fe4601dc4666728dd81c5fc964f556c3
IsAGi-auRa <itsaman24916@gmail.com>
2026-09-15 18:17:08 +0530

    remaining

--- 1_vitereact/src/main.jsx
+++ 1_vitereact/src/main.jsx
@@ -1,19 +1,20 @@
-import React, { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
 
-const anotherUser = "Itachi Uchiha"
+// const anotherUser = "Itachi Uchiha";
 
-const reactElement = React.createElement(
-  'a',
-  {href: 'https://google.com', target: '_blank'},
-  'click me to visit Google.',
-  anotherUser
-) 
+// const reactElement = React.createElement(
+//   "a",
+//   { href: "https://google.com", target: "_blank" },
+//   "click me to visit Google.",
+//   anotherUser,
+// );
 
 // -> this is how react render works similar to our customRender
 
-
-createRoot(document.getElementById('root')).render(
-    reactElement  
-)
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
